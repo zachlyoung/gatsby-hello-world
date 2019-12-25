499df56f6df873bf5c9a4f3e0693f57f6ad05369
Zachary Young <zachlyoung2022@gmail.com>
2019-12-24 22:05:37 -0500

    Finished part 4

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,13 @@
 import React from "react"
+import Layout from "../components/layout"
 import Header from "../components/header"
 
 export default () => (
-  <div style={{ color: `teal` }}>
-    <Header headerText="About Gatsby" />
-    <Header headerText="It's pretty cool" />
-    <p>Such wow. Very React.</p>
-  </div>
+    <Layout>
+        <Header headerText="About Gatsby" />
+        <Header headerText="It's pretty cool" />
+        <p>Such wow. Very React.</p>
+        <h1>About me</h1>
+        <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
+    </Layout>
 )
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
+import Layout from "../components/layout"
 import Header from "../components/header"
 
 export default () => (
-  <div style={{ color: `teal` }}>
+  <Layout>
     <Link to="/">Home</Link>
     <Header headerText="Contact" />
     <p>Send us a message!</p>
-  </div>
+    <h1>I'd love to talk! Email me at the address below</h1>
+    <p>
+      <a href="mailto:me@example.com">me@example.com</a>
+    </p>
+  </Layout>
 )
